6ce886ef5281456e64ec362eae81a518380334ad
sami <you@example.com>
2020-10-19 00:07:28 +0200

    addedOne

---
+++ main.js
@@ -0,0 +1,18 @@
+
+    window.addEventListener('scroll',function(){
+        var header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0);
+    });
+
+    function toggleMenu(){
+        var menuToggle = document.querySelector(".toggle");
+        menuToggle.classList.toggle("active")
+    }
+
+
+
+
+
+
+
+
